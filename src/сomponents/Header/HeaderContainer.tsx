952ff3914a69504c70d75fe194c89bd8e506bac9
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-07-31 11:11:39 +0300

    'header'

---
+++ src/сomponents/Header/HeaderContainer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getAuthUserData, InitStateType, logout } from '../../redux/auth-reducer';
+import { RootState } from '../../redux/redux-store';
+import { Header } from './Header'
+
+type HeaderApiContainerPropsType = {
+  data: InitStateType
+  getAuthUserData: () => void
+  logout: () => void
+};
+
+class HeaderApiContainer extends React.Component<HeaderApiContainerPropsType> {
+  componentDidMount() {
+    this.props.getAuthUserData();
+  };
+  render() {
+    return <Header
+      data={this.props.data}
+      logout={this.props.logout}
+    />
+  }
+};
+
+const mapStateToProps = (state: RootState) => ({
+  data: state.auth,
+  isAuth: state.auth.isAuth,
+  login: state.auth.login,
+});
+export const HeaderContainer = connect(mapStateToProps, { getAuthUserData, logout })(HeaderApiContainer);
