48d05f03faeb0a2b0a93c0d84cc6e28731e7a86b
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-07-31 11:07:07 +0300

    'dialigs'

---
+++ src/сomponents/Dialogs/AddMessageForm.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Field, InjectedFormProps, reduxForm } from 'redux-form';
+import { maxLengthCreators, required } from '../../utils/validators/validators';
+import { Textarea } from '../common/FormsControls/FormsControls';
+
+type FormDataType = {
+    newMessageBody: string
+}
+
+const maxLength50 = maxLengthCreators(50) 
+
+const AddMessageForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field
+                    component={Textarea}
+                    name='newMessageBody'
+                    placeholder='Enter your message'
+                    validate={[required, maxLength50]}
+
+                />
+                <div><button>Send</button></div>
+            </div>
+
+        </form>
+    )
+};
+
+export const AddMessageFormRedux = reduxForm<FormDataType>({ form: 'dialogAddMessageForm' })(AddMessageForm)
---
+++ src/сomponents/Dialogs/DialogsContainer.tsx
@@ -0,0 +1,42 @@
+import React, { } from 'react';
+import { DialogsType, InitialStateType, MessagesType, sendMessageC} from '../../redux/dialogs-reduser';
+import {connect} from 'react-redux'
+import { Dispatch } from 'redux';
+
+
+//   export type DialogsPageType = {
+//     messages: Array<MessagesType>
+//     dialogs: Array<DialogsType>
+//     newMessageBody: string
+//   };
+
+// type MapStatePropsType = {
+//     dialogsPage: DialogsPageType
+// }
+
+// type MapDispatchPropsType = {
+//     onNewMessageChange: () => void
+//     sendMessage: (body: string) => void
+// }
+
+// export type DialogsPropsType = MapStatePropsType & MapDispatchPropsType
+
+
+// const mapStateToProps = (state: DialogsPropsType): MapStatePropsType => {
+//     return {
+//         dialogsPage: state.dialogsPage
+//     }
+// }
+// const mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
+//     return {
+//         onNewMessageChange: () => { 
+//             dispatch(sendMessageC())
+//         },
+//         sendMessage: (body: string) => {
+//             dispatch(updateNewMessageC(body))
+//         }
+//     }
+// }
+
+
+// export const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs)
