b7d84aed42f431e88554e66bd4ec998dac23e434
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-18 11:06:13 +0300

    'auth'

--- src/redux/auth-reducer.tsx
+++ src/redux/auth-reducer.tsx
@@ -3,7 +3,7 @@ import { Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { authAPI } from '../api/api';
 import { AppStateType } from './redux-store';
-import {stopSubmit} from 'redux-form'
+import { stopSubmit } from 'redux-form'
 
 
 type ActionType = ReturnType<typeof setAuthUserData>
@@ -13,7 +13,7 @@ const SET_USER_DATA = 'SET-USER-DATA';
 export type InitStateType = {
     id: number | null,
     email: string | null,
-    login:  string | null,
+    login: string | null,
     isAuth: boolean,
 };
 
@@ -28,21 +28,21 @@ const initialState = {
 const authReducer = (state: InitStateType = initialState, action: ActionType): InitStateType => {
 
     switch (action.type) {
-        case SET_USER_DATA :
+        case SET_USER_DATA:
             return {
                 ...state,
                 ...action.payload,
             }
-        default: 
+        default:
             return state;
     };
 };
 
 
-export const setAuthUserData = ( id: number | null, email: string | null, login: string | null, isAuth: boolean) => {
+export const setAuthUserData = (id: number | null, email: string | null, login: string | null, isAuth: boolean) => {
     return {
         type: SET_USER_DATA,
-        payload: {id, email, login, isAuth}
+        payload: { id, email, login, isAuth }
     } as const
 };
 
@@ -72,7 +72,7 @@ export const login = (email: string, password: string, rememberMe: boolean) => (
 export const logout = () => (dispatch: Dispatch<any>) => {
     authAPI.logout()
         .then(response => {
-            if (response.data.resultCode === 0) {   
+            if (response.data.resultCode === 0) {
                 dispatch(setAuthUserData(null, null, null, false))
             }
         })
