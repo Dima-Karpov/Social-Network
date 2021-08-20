900da42b764129a7440e746d3eaeed198ef6dfb8
Dima-Karpov <dimka.carpov2013@yandex.ru>
2021-08-20 10:31:53 +0300

    'use-effect'

--- src/сomponents/Profile/ProfileInfo/ProfileStatuWithHooks.tsx
+++ src/сomponents/Profile/ProfileInfo/ProfileStatuWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 
 
 type ProfileStatuWithHooksPropsType = {
@@ -14,6 +14,10 @@ export const ProfileStatuWithHooks: React.FC<ProfileStatuWithHooksPropsType> = p
 
     const [editMode, setEditMode] = useState(false);
     const [newStatus, setStatus] = useState(status);
+
+    useEffect (() => {
+        setStatus(status)
+    }, [status]);
 
     const activeteEditMode = () => {
         setEditMode(true);
